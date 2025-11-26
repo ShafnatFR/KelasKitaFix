0bcc864185d2018597069a56492930a7937ca9b2
AliqNurShiddiq-TelU <aliqnurshiddiq@student.telkomuniversity.ac.id>
2025-11-26 13:49:56 +0700

    cek aliq

--- database/migrations/2025_11_23_174022_create_tb_laporan_table.php
+++ database/migrations/2025_11_23_174022_create_tb_laporan_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('keterangan_report');
             $table->enum('status_laporan', ['belum diproses', 'diproses', 'selesai', 'ditolak']);
             $table->foreignId('kelas_id')->constrained('tb_kelas')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('tb_users')->onDelete('cascade');
+            // $table->foreignId('user_id')->constrained('tb_users')->onDelete('cascade');
             $table->timestamps();
         });
     }
