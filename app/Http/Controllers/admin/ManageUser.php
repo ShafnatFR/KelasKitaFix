142475a631bc0cbf38b073952202486efd658c3f
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-11-26 14:21:06 -0800

    INISIALISASI

--- app/Http/Controllers/admin/ManageUser.php
+++ app/Http/Controllers/admin/ManageUser.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class ManageUser extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function createUser(Request $request){
+    
+    }
+
+    public function updateUser(Request $request){
+
+    }
+
+    public function deleteUser(Request $request){
+
+    }
+
+    public function getUser(Request $request){
+    
+    }
+
+    public function getUserDetail(Request $request){
+    
+    }
 }
--- app/Http/Controllers/admin/ViewData.php
+++ app/Http/Controllers/admin/ViewData.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class ViewData extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function totalCount(){
+
+    }
+
+    public function totalTransaction(){
+
+    }
+
+    public function totalCountTransaction(){
+
+    }
+
+    public function totalCountReport(){
+
+    }
+
+    public function totalTransactionReport(){
+
+    }
 }
